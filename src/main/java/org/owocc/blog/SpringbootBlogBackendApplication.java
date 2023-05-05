239577d2a0709fc22ea5ff4ab1dded2ab98cb52e
owocc <owocc@qq.com>
2023-05-05 09:49:28 +0800

    Initial commit

---
+++ src/main/java/org/owocc/blog/SpringbootBlogBackendApplication.java
@@ -0,0 +1,13 @@
+package org.owocc.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBlogBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBlogBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/owocc/blog/SpringbootBlogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.owocc.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBlogBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
